773440496bb7b438e9823d0ea9f9bd02eb800d10
hwoongg <gusdnd8536@gmail.com>
2020-12-03 11:21:41 +0900

    씬 오브젝트 기본 배치 및 2D UI Manager 추가

---
+++ Kiosk/Assets/Scripts/SceneManager/UI2DManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI2DManager : MonoBehaviour
+{
+    TitleManager tm;
+
+    Canvas[] canvasArr;
+    
+    void Start()
+    {
+        // 캔버스 카메라 지정 (Camera Space Canvas)
+        tm = FindObjectOfType<TitleManager>();
+        canvasArr = FindObjectsOfType<Canvas>();
+        for(int i=0; i<canvasArr.Length;i++)
+        {
+            canvasArr[i].worldCamera = Camera.main;
+            canvasArr[i].planeDistance = 0.4f;
+        }
+    }
+    
+    public void Close2D()
+    {
+        tm.Close2D();
+    }
+}
