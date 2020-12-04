047d212e5ae4a4a9082bbec4dcf105ed4d9249a2
hwoongg <gusdnd8536@gmail.com>
2020-12-04 13:44:16 +0900

    컨텐츠 페이지 전환 기능 추가 및 상호작용 컨텐츠 메뉴 프리팹 추가, 2D 윈도우 UI 갱신

---
+++ Kiosk/Assets/Scripts/PageGroup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity.Interaction;
+
+public class PageGroup : MonoBehaviour
+{
+    InteractionButton prevBtn;
+    InteractionButton nextBtn;
+
+    int pageIdx;
+    int pageCount;
+
+    int PageIdx
+    {
+        get
+        {
+            return pageIdx;
+        }
+
+        set
+        {
+            if (value < 0 || value >= pageCount)
+                return;
+            pageIdx = value;
+        }
+    }
+
+    private void OnEnable()
+    {
+        pageIdx = 0;
+    }
+
+    void Start()
+    {
+        // Get Button Ref
+        prevBtn = transform.parent.Find("Btn_PrevPage").GetComponent<InteractionButton>();
+        nextBtn = transform.parent.Find("Btn_NextPage").GetComponent<InteractionButton>();
+
+        // Mapping Events...
+        prevBtn.OnPress += PrevPage;
+        nextBtn.OnPress += NextPage;
+
+        pageCount = transform.childCount;
+    }
+    
+
+    void PrevPage()
+    {
+        PageIdx--;
+        SetPage();
+    }
+
+    void NextPage()
+    {
+        PageIdx++;
+        SetPage();
+    }
+
+    void SetPage()
+    {
+        for(int i=0; i<transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        transform.GetChild(pageIdx).gameObject.SetActive(true);
+    }
+}
